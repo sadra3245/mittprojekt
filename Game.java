a81f41cfd436f84223f1743774d6eaed9964f114
Sadra Javad <sadramohtork@gmail.com>
2025-10-14 14:21:47 +0200

    add console

--- Game.java
+++ Game.java
@@ -1,5 +1,16 @@
+import java.io.Console;
+
 public class Game {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+	System.out.println("Welcome to the Countries game.");
+	Console console = System.console();
+	String answer = console.readLine("Which continent does Sweden belong to? ");
+	String continent= "Europe";
+if (answer.equals(continent)) {
+    System.out.println("Correct!");
+} else {
+   System.out.println("No, the answer is " + continent);
+}
     }
 }
