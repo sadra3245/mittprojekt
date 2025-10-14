5fe95fabb11caf8d21e1e7fe3e0d2ff3f33616fb
Sadra Javad <sadramohtork@gmail.com>
2025-10-14 13:57:47 +0200

    ignore the case

--- Game.java
+++ Game.java
@@ -18,7 +18,7 @@ public class Game {
 		String country = cols[0];
 		String continent= cols[1];
 		String answer= console.readLine("which continent does " + country+ " belong to?");
-	if(answer.equals(continent)) {
+	if(answer.equalsIgnoreCase(continent)) {
 	    System.out.println("Correct!");
 	}else {
 	    System.out.println("No, the answer is " + continent);
